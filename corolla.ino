8f92c4f4fd11fc228f4edb4dfa2c251592fc19e1
sebathorus <sebathorus@yahoo.com>
2017-03-28 20:49:15 +0300

    Add files via upload

--- corolla.ino
+++ corolla.ino
@@ -18,6 +18,14 @@ int pot1Pin = A2;//used to change petals position
 int pot2Pin = A1;//used to change light intensity
 int pot3Pin = A0;//used to change color of RGB Leds
 int feedbackpotPin = A3;//used to determine petals position
+
+int switch1Pin1 = 2;
+int switch1Pin2 = 4;
+int switch1Pin3 = 5;
+int switch1val1 = 0;
+int switch1val2 = 0;
+int switch1val3 = 0;
+
 int pot1RawValue = 0;  //variables to store the values coming from the potentiometers
 int pot2RawValue = 0;
 int pot3RawValue = 0;
@@ -28,11 +36,15 @@ int feedbackpotValue = 0;
 int stepper_turn = 0;
 
 void setup() {
+  // declare the ledPin as an OUTPUT:
   pinMode(myledPin, OUTPUT);
   pinMode(pot1Pin, INPUT);
   pinMode(pot2Pin, INPUT);
   pinMode(pot3Pin, INPUT);
   pinMode(feedbackpotPin, INPUT);
+  pinMode(switch1Pin1, INPUT);
+  pinMode(switch1Pin2, INPUT);
+  pinMode(switch1Pin3, INPUT);
   strip.begin();//start RGB LEDs
   strip.show();
   Serial.begin(9600);
@@ -53,9 +65,9 @@ void loop() {
   pot2RawValue = analogRead(pot2Pin);
   pot3RawValue = analogRead(pot3Pin);
 
-  pot1Value = pot1RawValue;
-  pot2Value = map(pot2RawValue, 0, 1023, 0, 250);
-  pot3Value = map(pot3RawValue, 0, 1023, 0, 20);
+  switch1val1 = digitalRead(switch1Pin1);
+  switch1val2 = digitalRead(switch1Pin2);
+  switch1val3 = digitalRead(switch1Pin3);
   
   feedbackpotValue = analogRead(feedbackpotPin);
   Serial.print("Pot1 = ");
@@ -67,39 +79,95 @@ void loop() {
   Serial.print("Feedbackpot = ");
   Serial.println(feedbackpotValue);
 
-  if (pot1Value < 300)//if the value read from the first potentiometer, pot1, is below this value, petals are closing
+  if ((switch1val1 == 0)&&(switch1val2 == 0)&&(switch1val3 == 1))//start switch1 position1
     {
-      stepper_turn = - oneOutputTurn;
-      Serial.println("Close");
-    }
-  if ((pot1Value < 300) && (feedbackpotValue < 690))//if the value read from the feedback potentiometer, is below this value (690 in this case), stepper is stopped to not damage the mechanics and/or motor
-    {
-      stepper_turn = 0;
-      Serial.println("Close stopped");
-    }
-  
-  if (pot1Value > 600)//if the value read from the first potentiometer, pot1, is above this value, petals are opening
-    {
-      stepper_turn = oneOutputTurn;
-      Serial.println("Open");
-    }
-  if ((pot1Value > 600) && (feedbackpotValue > 750))//if the value read from the feedback potentiometer, is above this value (750 in this case), stepper is stopped to not damage the mechanics and/or motor
-    {
-      stepper_turn = 0;
-      Serial.println("Open stopped");
-    }
-  
-  if ((pot1Value > 300) && (pot1Value < 600))
-    {
-      stepper_turn = 0;
-      Serial.println("Stepper stop");
-    }
-  motor_stepper.setSpeed(500);   
-  motor_stepper.step(stepper_turn);
 
-  red_led = pot2Value;
-  green_led = pot2Value;
-  blue_led = pot2Value/pot3Value;
-  colorWipe(strip.Color(red_led, green_led, blue_led), 50);
-  delay(50);
-}
+      pot1Value = map(pot1RawValue, 0, 1023, 0, 250);
+      pot2Value = map(pot2RawValue, 0, 1023, 0, 42);
+      pot3Value = map(pot3RawValue, 0, 1023, 0, 15);
+      
+      if (pot1RawValue < 300)//if the value read from the first potentiometer, pot1, is below this value, petals are closing
+        {
+          stepper_turn = - oneOutputTurn;
+          Serial.println("Close");
+        }
+      if ((pot1RawValue < 300) && (feedbackpotValue < 680))//if the value read from the feedback potentiometer, is below this value (690 in this case), stepper is stopped to not damage the mechanics and/or motor
+        {
+          stepper_turn = 0;
+          Serial.println("Close stopped");
+        }
+  
+      if (pot1RawValue > 600)//if the value read from the first potentiometer, pot1, is above this value, petals are opening
+        {
+          stepper_turn = oneOutputTurn;
+          Serial.println("Open");
+        }
+      if ((pot1RawValue > 600) && (feedbackpotValue > 750))//if the value read from the feedback potentiometer, is above this value (750 in this case), stepper is stopped to not damage the mechanics and/or motor
+        {
+          stepper_turn = 0;
+          Serial.println("Open stopped");
+        }
+  
+      if ((pot1RawValue > 300) && (pot1RawValue < 600))
+       {
+          stepper_turn = 0;
+          Serial.println("Stepper stop");
+       }
+  
+      motor_stepper.setSpeed(500);   
+      motor_stepper.step(stepper_turn);
+
+      red_led = (pot2Value * 6) - pot3Value;
+      green_led = (pot2Value * 5) - pot3Value;
+      blue_led = pot2Value + pot3Value;
+      colorWipe(strip.Color(green_led, red_led, blue_led), 30);
+    }//end switch1 position1
+
+  if ((switch1val1 == 0)&&(switch1val2 == 1)&&(switch1val3 == 1))//start switch1 position 2
+    {
+
+      pot1Value = map(pot1RawValue, 0, 1023, 0, 252);
+      pot2Value = map(pot2RawValue, 0, 1023, 0, 252);
+      pot3Value = map(pot3RawValue, 0, 1023, 0, 252);
+
+      Serial.print("Pot1 = ");
+      Serial.println(pot1Value);
+      Serial.print("Pot2 = ");
+      Serial.println(pot2Value);
+      Serial.print("Pot3 = ");
+      Serial.println(pot3Value);
+      
+      red_led = pot1Value;
+      green_led = pot2Value;
+      blue_led = pot3Value;
+      colorWipe(strip.Color(green_led, red_led, blue_led), 30);
+    }//end switch1 position2
+
+  if ((switch1val1 == 0)&&(switch1val2 == 1)&&(switch1val3 == 0))//start switch1 position 3
+    {
+      Serial.println("Mode 3 selected");
+      pot1Value = map(pot1RawValue, 0, 1023, 0, 252);
+      pot2Value = map(pot2RawValue, 0, 1023, 0, 252);
+      pot3Value = map(pot3RawValue, 0, 1023, 0, 252);
+      Serial.print("Pot1 = ");
+      Serial.println(pot1Value);
+      Serial.print("Pot2 = ");
+      Serial.println(pot2Value);
+      Serial.print("Pot3 = ");
+      Serial.println(pot3Value);
+      delay(500);
+    }//end switch1 position 3
+
+  if ((switch1val1 == 1)&&(switch1val2 == 1)&&(switch1val3 == 0))//start switch1 position 4
+    {
+      Serial.println("Mode 4 selected");
+      delay(500);
+    }//end switch1 position 4
+
+  if ((switch1val1 == 1)&&(switch1val2 == 0)&&(switch1val3 == 0))//start switch1 position 5
+    {
+      Serial.println("Mode 5 selected");
+      delay(500);
+    }//end switch1 position 5
+    
+}//end loop
