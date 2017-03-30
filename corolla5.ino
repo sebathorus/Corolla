351b1954f0f333b68eb527e12244293a9036143e
sebathorus <sebathorus@yahoo.com>
2017-03-31 01:09:35 +0300

    Add files via upload

--- corolla5.ino
+++ corolla5.ino
@@ -39,6 +39,7 @@ int switch1Pin3 = 5;
 int switch1val1 = 0;
 int switch1val2 = 0;
 int switch1val3 = 0;
+int selectedMode = 1;
 
 int pot1RawValue1, pot1RawValue2, pot1RawValue3;  //variables to store the values coming from the potentiometers
 int pot2RawValue1, pot2RawValue2, pot2RawValue3;
@@ -82,6 +83,14 @@ void loop() {
   switch1val1 = digitalRead(switch1Pin1);// read the value from the switch
   switch1val2 = digitalRead(switch1Pin2);
   switch1val3 = digitalRead(switch1Pin3);
+  Serial.print("pot1RawValue1 = ");
+  Serial.println(pot1RawValue1);
+  Serial.print("pot1RawValue2 = ");
+  Serial.println(pot1RawValue2);
+  Serial.print("pot1RawValue3 = ");
+  Serial.println(pot1RawValue3);
+  Serial.print("Selected mode = ");
+  Serial.println(selectedMode);
     
   if ((switch1val1 == 0)&&(switch1val2 == 0)&&(switch1val3 == 1))//start switch1 position1
     {
@@ -91,9 +100,25 @@ void loop() {
       feedbackpotValue = analogRead(feedbackpotPin);
       Serial.print("Feedbackpot = ");
       Serial.println(feedbackpotValue);
-
-      if ((pot1RawValue1 < pot1RawValue2-20) || (pot1RawValue1 > pot1RawValue2+20) || (pot2RawValue1 < pot2RawValue2-20) || (pot2RawValue1 > pot2RawValue2+20) || (pot3RawValue1 < pot3RawValue2-20) || (pot3RawValue1 > pot3RawValue2+20))//I put this condition to not change the Mode parameters when switch from other Mode
-          {      
+      lcd.clear();
+      lcd.setCursor(5,0);//coordinates are (column, row) - 0,0 is upper left
+      lcd.write("Mode 1");
+      if (selectedMode == 2)
+          {
+            if ((pot1RawValue1 < pot1RawValue2-20) || (pot1RawValue1 > pot1RawValue2+20) || (pot2RawValue1 < pot2RawValue2-20) || (pot2RawValue1 > pot2RawValue2+20) || (pot3RawValue1 < pot3RawValue2-20) || (pot3RawValue1 > pot3RawValue2+20))//I put this condition to not change the Mode parameters when switch from other Mode
+              {
+                selectedMode = 1;
+              }
+          }
+       if (selectedMode == 3)
+          {
+            if ((pot1RawValue1 < pot1RawValue3-20) || (pot1RawValue1 > pot1RawValue3+20) || (pot2RawValue1 < pot2RawValue3-20) || (pot2RawValue1 > pot2RawValue3+20) || (pot3RawValue1 < pot3RawValue3-20) || (pot3RawValue1 > pot3RawValue3+20))//I put this condition to not change the Mode parameters when switch from other Mode
+              {
+                selectedMode = 1;
+              }
+          }
+        if (selectedMode == 1)
+          {
             petalPos = map(pot1RawValue1, 0, 1023, 0, 250);
             brightness = map(pot2RawValue1, 0, 1023, 0, maxbrightness);
             colorTemp = map(pot3RawValue1, 0, 1023, 0, 15);
@@ -103,7 +128,7 @@ void loop() {
             Serial.println(brightness);
             Serial.print("colorTemp = ");
             Serial.println(colorTemp);
-          }
+          //}
             if (pot1RawValue1 < 300)//if the value read from the first potentiometer, pot1, is below this value, petals are closing
               {
                 stepper_turn = - halfOutputTurn;
@@ -139,7 +164,8 @@ void loop() {
             green_led = (brightness * green_ratio) - colorTemp;
             blue_led = (brightness * blue_ratio) + colorTemp;
             colorWipe(strip.Color(green_led, red_led, blue_led), 30);
-          
+        }
+     delay(300);
     }//end switch1 position1
 
   if ((switch1val1 == 0)&&(switch1val2 == 1)&&(switch1val3 == 1))//start switch1 position 2
@@ -147,13 +173,29 @@ void loop() {
       pot1RawValue2 = analogRead(pot1Pin);// read the value from the potentiometers
       pot2RawValue2 = analogRead(pot2Pin);
       pot3RawValue2 = analogRead(pot3Pin);
+      lcd.clear();
+      lcd.setCursor(5,0);//coordinates are (column, row) - 0,0 is upper left
+      lcd.write("Mode 2");
       
-      if ((pot1RawValue2 < pot1RawValue1-20) || (pot1RawValue2 > pot1RawValue1+20) || (pot2RawValue2 < pot2RawValue1-20) || (pot2RawValue2 > pot2RawValue1+20) || (pot3RawValue2 < pot3RawValue1-20) || (pot3RawValue2 > pot3RawValue1+20))
-        {
-          ledPot1Value = map(pot1RawValue2, 0, 1023, 1, 252);
-          ledPot2Value = map(pot2RawValue2, 0, 1023, 1, 252);
-          ledPot3Value = map(pot3RawValue2, 0, 1023, 1, 252);
-        }
+      if (selectedMode == 1)
+          {
+            if ((pot1RawValue2 < pot1RawValue1-20) || (pot1RawValue2 > pot1RawValue1+20) || (pot2RawValue2 < pot2RawValue1-20) || (pot2RawValue2 > pot2RawValue1+20) || (pot3RawValue2 < pot3RawValue1-20) || (pot3RawValue2 > pot3RawValue1+20))
+              {
+                selectedMode = 2;
+              }
+          }
+       if (selectedMode == 3)
+          {
+            if ((pot1RawValue2 < pot1RawValue3-20) || (pot1RawValue2 > pot1RawValue3+20) || (pot2RawValue2 < pot2RawValue3-20) || (pot2RawValue2 > pot2RawValue3+20) || (pot3RawValue2 < pot3RawValue3-20) || (pot3RawValue2 > pot3RawValue3+20))
+              {
+                selectedMode = 2;
+              }
+          }
+        if (selectedMode == 2)
+          {
+            ledPot1Value = map(pot1RawValue2, 0, 1023, 1, 252);
+            ledPot2Value = map(pot2RawValue2, 0, 1023, 1, 252);
+            ledPot3Value = map(pot3RawValue2, 0, 1023, 1, 252);
           /*Serial.print("Pot1 = ");
           Serial.println(pot1Value);
           Serial.print("Pot2 = ");
@@ -164,9 +206,9 @@ void loop() {
           green_led = ledPot2Value;
           blue_led = ledPot3Value;
     
-          lcd.clear();
-          lcd.setCursor(5,0);//coordinates are (column, row) - 0,0 is upper left
-          lcd.write("Mode 2");
+          //lcd.clear();
+          //lcd.setCursor(5,0);//coordinates are (column, row) - 0,0 is upper left
+          //lcd.write("Mode 2");
           lcdString = "R" + String(red_led) + " G " + String(green_led) + " B " + String(blue_led);
           lcd.setCursor(0,1);
           lcd.print(lcdString);
@@ -230,8 +272,8 @@ void loop() {
           Serial.println(maxRatio);
           Serial.print("Max brightness = ");
           Serial.println(maxbrightness);*/
-          delay(300);
-        
+         }
+      delay(300);
     }//end switch1 position2
 
   if ((switch1val1 == 0)&&(switch1val2 == 1)&&(switch1val3 == 0))//start switch1 position 3
@@ -245,19 +287,38 @@ void loop() {
       lcdString = "R" + String(red_lcd) + " G " + String(green_lcd) + " B " + String(blue_lcd);
       lcd.setCursor(0,1);
       lcd.print(lcdString);
-      
-      Serial.println("Mode 3 selected");
-      red_lcd = map(pot1RawValue3, 0, 1023, 0, 252);
-      green_lcd = map(pot2RawValue3, 0, 1023, 0, 252);
-      blue_lcd = map(pot3RawValue3, 0, 1023, 0, 252);
-      Serial.print("Red LCD = ");
-      Serial.println(red_lcd);
-      Serial.print("Green LCD = ");
-      Serial.println(green_lcd);
-      Serial.print("Blue LCD = ");
-      Serial.println(blue_lcd);
-      lcd.setRGB(red_lcd, green_lcd, blue_lcd);
-      delay(500);
+
+      if (selectedMode == 1)
+          {
+            if ((pot1RawValue3 < pot1RawValue1-20) || (pot1RawValue3 > pot1RawValue1+20) || (pot2RawValue3 < pot2RawValue1-20) || (pot2RawValue3 > pot2RawValue1+20) || (pot3RawValue3 < pot3RawValue1-20) || (pot3RawValue3 > pot3RawValue1+20))
+              {
+                selectedMode = 3;
+              }
+          }
+      if (selectedMode == 2)
+          {
+            if ((pot1RawValue3 < pot1RawValue2-20) || (pot1RawValue3 > pot1RawValue2+20) || (pot2RawValue3 < pot2RawValue2-20) || (pot2RawValue3 > pot2RawValue2+20) || (pot3RawValue3 < pot3RawValue2-20) || (pot3RawValue3 > pot3RawValue2+20))
+              {
+                selectedMode = 3;
+              }
+          }
+      if (selectedMode == 3)
+          {       
+                Serial.println("Mode 3 selected");
+                red_lcd = map(pot1RawValue3, 0, 1023, 1, 252);
+                green_lcd = map(pot2RawValue3, 0, 1023, 1, 252);
+                blue_lcd = map(pot3RawValue3, 0, 1023, 1, 252);
+                Serial.print("Red LCD = ");
+                Serial.println(red_lcd);
+                Serial.print("Green LCD = ");
+                Serial.println(green_lcd);
+                Serial.print("Blue LCD = ");
+                Serial.println(blue_lcd);
+                lcd.setRGB(red_lcd, green_lcd, blue_lcd);
+                delay(500);
+                selectedMode = 3;
+             }
+      delay(300);
     }//end switch1 position 3
 
   if ((switch1val1 == 1)&&(switch1val2 == 1)&&(switch1val3 == 0))//start switch1 position 4
